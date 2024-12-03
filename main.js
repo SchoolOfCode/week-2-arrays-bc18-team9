7cba64c9a6c08a249b2fc2b5644015190726be18
Maddy <184935886+Maddy-bee@users.noreply.github.com>
2024-12-03 21:44:25 +0000

    Start Ticket 4
    Remaining work is to find method to multiple the properties of the array
    by 10 and log these new values.

--- main.js
+++ main.js
@@ -7,3 +7,21 @@ console.log(Array.isArray(songs))
 console.log(songs[1])
 
 console.log(songs[0])
+
+//You've decided you no longer like your fifth song and the song 'Baby Shark' has now become your fifth favourite.
+// Use bracket notation to access the item and use the assignment operator to reassign it to 'Baby Shark'. 
+//Then log the array to the console to check that the fifth song is now 'Baby Shark' 🎉
+
+songs[4] = "Baby Shark"
+
+console.log(songs[4])
+
+
+let cookiesOrdered = [15, 3, 67, 1, 22, 100];
+
+for (let i = 0; i < cookiesOrdered.length; i++) {
+    
+
+}
+
+console.log(cookiesOrdered)
