526895841e75590d596bc4087a0b2b67ecbba457
Maddy <184935886+Maddy-bee@users.noreply.github.com>
2024-12-03 18:38:44 +0000

    Complete Tickets 1 2 and 3
    Testing and understanding array syntax, length and
    selecting using indices.

---
+++ main.js
@@ -0,0 +1,9 @@
+let songs = ["Free", "Us", "TV Off", "Sitting on the dock of the bay", "Happy"]
+
+console.log(songs.length)
+
+console.log(Array.isArray(songs))
+
+console.log(songs[1])
+
+console.log(songs[0])
